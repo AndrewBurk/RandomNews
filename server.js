4f5353ce6652bd27e9fd3d0f2aad11fa133d66d6
AndrewBurk <burkmag@gmail.com>
2015-10-07 09:06:27 +0000

    Improve of logging added new logic of config module.

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 // set up ======================================================================
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
-var mongoose = require('mongoose'); 					// mongoose for mongodb
-var port  	 = process.env.PORT || 8080; 				// set the port
-var database = require('./config/database'); 			// load the database config
-var morgan   = require('morgan');
+var mongoose = require('mongoose'); 					// mongoose for mongodb		
+var config = require('./config'); 			// load the database config
+var log   = require('./lib/log')(module);
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
+if (app.get('env') == 'development') {
+  app.use(express.logger('dev'));
+} else {
+  app.use(express.logger('default'));
+}
+
 // configuration ===============================================================
-mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
+mongoose.connect(config.get('db:uri'), config.get('db:options')); 	// connect to mongoDB database on modulus.io
 
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
-app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
@@ -23,5 +27,5 @@ app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-M
 require('./app/routes.js')(app);
 
 // listen (start app with node server.js) ======================================
-app.listen(port);
-console.log("App listening on port " + port);
+app.listen(config.get('port'), function(){
+        log.info("App listening on port " + config.get('port'))});
